5ec7af74d782b8a97cb71a7317eba8ecc32d6207
Matthew Gwynne <thegwynne@gmail.com>
2018-08-22 13:59:24 +0100

    edited python again

--- pythonspeedtest.py
+++ pythonspeedtest.py
@@ -1,8 +1,10 @@
 import time
+from math import sqrt
+import random
 
 starttime = time.time()
 counter = 0
 for i in range(10000000):
-    a = 89 ** 13
+    a = sqrt(random.random())
     counter += 1
 print(time.time() - starttime)
